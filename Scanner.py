8a0375d57ad2154b1d062ad9972bb8b6f0bcd85a
LeviantCoder <joshruss09@gmail.com>
2022-02-08 14:36:28 +0000

    Enter

---
+++ Scanner.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import nmap
+
+scanner = nmap.PortScanner()
+
+print("Welcome, this is a basic NMAP automation tool")
+print("<---------------------------------------------------->)")
+
+ip_addr = input("Please enter IP address to be scanned")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
+
+resp = input(""" \nPlease enter the type of scan you want to run
+                 1)SYN ACK Scan
+                 2)UDP Scan
+                 3)Comprehensive Scan \n""")
+print("You have selected option: ", resp)
+
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip-addr, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print("IP status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+elif resp == '2':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip-addr, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print("IP status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
+elif resp == '3':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip-addr, '1-1024', '-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print("IP status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+elif resp >= '4':
+    print("Please enter valid option")    
